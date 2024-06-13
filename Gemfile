a781026a597263b8fce86c461952f418eaf430ff
Naresh Verma <learnnaresh7566@gmail.com>
2024-06-13 16:17:02 +0530

    Add smtp configuration

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'sidekiq'
 
 gem 'redis'
 
+gem 'dotenv'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
@@ -57,7 +59,6 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
   gem 'rspec-rails'
-  gem 'byebug'
 end
 
 group :development do
@@ -78,3 +79,4 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.6"
+gem 'byebug'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,19 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    user_name: ENV['SMTP_MAIL'],
+    password: ENV['SMTP_PASS'],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
+
+  config.action_mailer.preview_path = Rails.root.join('test/mailers/previews')
 end
